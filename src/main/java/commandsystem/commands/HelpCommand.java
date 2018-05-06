86db12a683a99b023cd561333c72e92611d45603
Ömer <o.erarsl.oe@gmail.com>
2018-05-06 19:51:12 +0200

    Help Command

--- src/main/java/commandsystem/commands/HelpCommand.java
+++ src/main/java/commandsystem/commands/HelpCommand.java
@@ -15,15 +15,28 @@ public class HelpCommand extends Command{
 
     @Override
     public void action(String[] args, MessageReceivedEvent event) {
-        if(args[1] == ""){
-            EmbedBuilder builder = new EmbedBuilder();
-            builder.setColor(Color.ORANGE);
-            builder.setFooter("Requested by " + event.getAuthor().getName(), event.getAuthor().getAvatarUrl());
-            builder.setDescription(
-                    CommandManager.commands.get(0).getName() + " " + CommandManager.commands.get(0).getDescription() + "\n" +
-                    CommandManager.commands.get(1).getName() + " " + CommandManager.commands.get(0).getDescription());
-            event.getTextChannel().sendMessage(builder.build()).queue();
-        }
+            if(args[1] != null){
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setTitle("Commandlist");
+                builder.setColor(Color.ORANGE);
+                builder.setFooter("Requested by " + event.getAuthor().getName(), event.getAuthor().getAvatarUrl());
+                for(Command c : CommandManager.commands){
+                    if(args[1].equalsIgnoreCase(c.getName())){
+                        builder.setDescription(c.getName() + " " + c.getDescription());
+                    }
+                }
+                event.getTextChannel().sendMessage(builder.build()).queue();
+            }else{
+                EmbedBuilder builder = new EmbedBuilder();
+                builder.setTitle("Commandlist");
+                builder.setColor(Color.ORANGE);
+                builder.setFooter("Requested by " + event.getAuthor().getName(), event.getAuthor().getAvatarUrl());
+                builder.setDescription(
+                        CommandManager.commands.get(0).getName() + " " + CommandManager.commands.get(0).getDescription() + "\n" +
+                                CommandManager.commands.get(1).getName() + " " + CommandManager.commands.get(1).getDescription()
+                );
+                event.getTextChannel().sendMessage(builder.build()).queue();
+            }
     }
 
     @Override
