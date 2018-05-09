3d60299192dad8dfdf28622c567a56773bcd6a02
R3tuxn <35817110+R3tuxn@users.noreply.github.com>
2018-05-09 20:52:33 +0200

    Test

--- src/main/java/listener/ReadyListener.java
+++ src/main/java/listener/ReadyListener.java
@@ -4,11 +4,18 @@ import logging.Logger;
 import net.dv8tion.jda.core.events.ReadyEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 
+import java.sql.Time;
+import java.time.Clock;
+import java.time.Instant;
+import java.time.OffsetTime;
+import java.time.ZoneId;
+
 public class ReadyListener extends ListenerAdapter{
 
     @Override
     public void onReady(ReadyEvent event) {
-        Logger.positLog("Bot started working at " + System.currentTimeMillis());
+
+        Logger.positLog("Bot started working at " + OffsetTime.now().getHour() + ":0" + OffsetTime.now().getMinute() + ":" + OffsetTime.now().getSecond());
         Logger.informationLog("Working on following servers: " + event.getJDA().getGuilds() + "\n");
     }
 }
